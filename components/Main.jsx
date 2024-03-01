fdb398791bbc76ac717c0babeae03790b5f022c7
Raj Naik <raj44.naik@gmail.com>
2024-03-01 15:58:25 +0530

    latest fix

--- components/Main.jsx
+++ components/Main.jsx
@@ -8,7 +8,7 @@ export const Main = () => {
     <div id="home" className="w-full h-screen text-center">
         <div className="max-w-[1240px] w-full h-full mx-auto p-2 flex justify-center items-center">
             <div>
-            <p className="uppercase text-sm tracking-widest text-gray-600">LET"S BUILD SOMETHING TOGEATHER</p>
+            <p className="uppercase text-sm tracking-widest text-gray-600">LET&apos;S BUILD SOMETHING TOGEATHER</p>
             <h1 className="py-4 text-gray-700">
             Hi, I&apos;m <span className="text-[#5651e5]">Raj</span>
             </h1>
